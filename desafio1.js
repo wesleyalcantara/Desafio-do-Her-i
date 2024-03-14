94d4f8acd6c321bce2533e4fa44e03fed1a59dc9
Wesley Batista Alcantara <wesleydevadm@gmail.com>
2024-03-14 20:21:29 -0300

    Desafio finalizado.

--- desafio1.js
+++ desafio1.js
@@ -1,29 +1,25 @@
+var niveis = ['Ferro', 'Bronze', 'Prata', 'Ouro', 'Platina', 'Ascendente', 'Imortal', 'Radiante'];
+var nivel;
 
-var niveis = ['Ferro', 'Bronze', 'Prata', 'Ouro', 'Platina', 'Ascendente', 'Imortal', 'Radiante']
-var nivel = ''
-
-function nivelDoHeroi (nome, xp) {
-    for (let i = 0; i < niveis.length; i++) {
-        if (xp > 0 && xp <= 1000) {
-            nivel = niveis[0];
-        } else if (xp > 1001 && xp <= 2000) {
-            nivel = niveis[1];
-        } else if (xp > 2001 && xp <= 5000) {
-            nivel = niveis[2];
-        } else if (xp > 5001 && xp <= 7000) {
-            nivel = niveis[3];
-        } else if (xp > 7001 && xp <= 8000) {
-            nivel = niveis[4];
-        } else if (xp > 8001 && xp <= 9000) {
-            nivel = niveis[5];
-        } else if (xp > 9001 && xp <= 10000) {
-            nivel = niveis[6];
-        } else {
-            nivel = niveis[7];
-        }
+function nivelDoHeroi(nome, xp) {
+    if (xp > 0 && xp <= 1000) {
+        nivel = niveis[0];
+    } else if (xp > 1000 && xp <= 2000) {
+        nivel = niveis[1];
+    } else if (xp > 2000 && xp <= 5000) {
+        nivel = niveis[2];
+    } else if (xp > 5000 && xp <= 7000) {
+        nivel = niveis[3];
+    } else if (xp > 7000 && xp <= 8000) {
+        nivel = niveis[4];
+    } else if (xp > 8000 && xp <= 9000) {
+        nivel = niveis[5];
+    } else if (xp > 9000 && xp <= 10000) {
+        nivel = niveis[6];
+    } else {
+        nivel = niveis[7];
     }
-
-    console.log(`O Herói de nome ${nome} está no nível ${nivel}`)
+    console.log(`O Herói de nome ${nome} está no nível ${nivel}`);
 }
 
-nivelDoHeroi('Wesley', 500)
+nivelDoHeroi('Wesley', 2500);
